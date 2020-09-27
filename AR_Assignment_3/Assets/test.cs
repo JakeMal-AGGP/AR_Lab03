8d900b1f99a28d2df242374da32f5a1e65fd9521
JakeMal-AGGP <62573128+JakeMal-AGGP@users.noreply.github.com>
2020-09-26 20:24:48 -0400

    initial commit

---
+++ AR_Assignment_3/Assets/test.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class test : MonoBehaviour
+{
+    public Button btn;
+
+
+    public void touched()
+    {
+        btn.gameObject.SetActive(false);
+    }
+}
